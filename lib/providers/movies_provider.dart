393bda39bbd975d8bc9f824aa393b1c376cc6146
Bryan Zapata <bmzc95@gmail.com>
2022-01-29 13:33:53 -0500

    feat: movies now playing

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:movies/models/models.dart';
 
 class MoviesProvider extends ChangeNotifier {
-  MoviesProvider() {}
+  final String _baseURL = 'api.themoviedb.org';
+  final String _apiKEY = '37127f7dd60eb91f9d037d0e5efc59d9';
+  final String _language = 'es-ES';
 
-  getOnDisplayMovies() {}
+  List<Movie> onDisplayMovies = [];
+
+  MoviesProvider() {
+    getOnDisplayMovies();
+  }
+
+  getOnDisplayMovies() async {
+    var url = Uri.https(_baseURL, '/3/movie/now_playing',
+        {'api_key': _apiKEY, 'language': _language, 'page': '1'});
+
+    final response = await http.get(url);
+    final newPlayingResponse = NowPlayingResponse.fromJson(response.body);
+
+    onDisplayMovies = newPlayingResponse.results;
+
+    notifyListeners();
+  }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:movies/providers/providers.dart';
 import 'package:movies/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,6 +9,8 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final moviesProvider = Provider.of<MoviesProvider>(context);
+
     return Scaffold(
       appBar: AppBar(
           title: const Text('Peliculas en cines'),
@@ -14,11 +19,9 @@ class HomeScreen extends StatelessWidget {
             IconButton(onPressed: () {}, icon: const Icon(Icons.search))
           ]),
       body: SingleChildScrollView(
-          child: Column(children: const [
-        CardSwiper(),
-        MovieSlider(),
-        MovieSlider(),
-        MovieSlider()
+          child: Column(children: [
+        CardSwiper(movies: moviesProvider.onDisplayMovies),
+        const MovieSlider(),
       ])),
     );
   }
